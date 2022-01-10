c693f76b40b87e8dd1dbc3bb3d93e66ba4ff9b13
PaoMo <paomo5515@qq.com>
2022-01-10 14:50:17 +0800

    init app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react'
+import ButtonSize from './ant_components/ButtonSize'
+import './App.css';
+
+export default class App extends Component {
+    render() {
+        //批量传递
+        return (
+            <div>
+                <ButtonSize />
+            </div>
+        )
+    }
+}
+
+
+
